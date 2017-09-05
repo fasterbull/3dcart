d140d73a3185c485ba81a77996471d6674aa96eb
Przemek Pawluk <pe-pe@o2.pl>
2017-09-05 23:27:42 +0200

    fixes

---
+++ consolevault/templates/consolevaultV2-html5/styles/temp.js
@@ -0,0 +1,7 @@
+<script type = "text/javascript">
+    jQuery(document).ready(function (e) {
+        jQuery('input[name="shipping_zip"]').click(function () {
+            jQuery('.mobile-search-container').toggleClass('shown');
+        });
+    });
+</script>
